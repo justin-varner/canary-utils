a7b11b4f414dbd62e21c15d7f0f2c89b89bf6fd9
Justin Varner <61245735+justin-varner@users.noreply.github.com>
2022-08-30 17:19:05 -0400

    Update syntax

--- Canary-GreyNoise-Enterprise-Threat-Intel-Report.sh
+++ Canary-GreyNoise-Enterprise-Threat-Intel-Report.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
-# This script pulls all alert data from a Canary outside bird, sends the results to the GreyNoise Enterprise API for detailed threat intelligence context, and creates a JSON report.
-# Please consult the README for prequisites and usage instructions
+# Canarytoken-GreyNoise-Enterprise-Threat-Intel-Report.sh
+# Justin Varner
+# August 30, 2022
 
-# Cleanup any residual files that may have been left behind from previously running the script
-rm -rf outside* canary-ips-*.txt
+# Fetch all incidents and filter out only IP addresses. Then write to a text file
+curl https://$CANARY_HASH.canary.tools/api/v1/incidents/all -d auth_token=$CANARY_TOKEN -G | jq '.' | grep -o '"src_host": "[^"]*' | grep -o '[^"]*$' | sort -u > canary-ips-$(date +%Y-%m-%d).txt
 
-# Query the Canary API for all outside bird events, extract and sort IPs, remove double quotes, and write to a text file for GreyNoise to contextualize
-curl -XGET "https://$CANARY_HASH.canary.tools/api/v1/incidents/outside_bird/download/json" \
-  -d auth_token=$CANARY_API_KEY \
-  -d node_id=$BIRD_ID \
-  -G -O -J \
-  && unzip outside_bird_alerts.json.zip \
-  && cat outside-bird-$BIRD_ID.json | jq '.[].ip_address' | sed 's/\"//g' | sort > canary-ips-$(date +%Y-%m-%d).txt
-
-# Read through each line of the canary text file, send each IP through the GreyNoise Enterprise API, and create a detailed JSON report
+## Read all IPs from a text file and run through Shodan for threat intel
 file="canary-ips-$(date +%Y-%m-%d).txt"
 lines=$(cat $file)
 
+{
 for line in $lines
-do
+do >/dev/null 2>&1
     curl -XGET "https://api.greynoise.io/v2/noise/context/$line" \
-      -H "key: $GREYNOISE_API_KEY" \
-      -H "accept: application/json" | jq '.' | cat >> Canary-GreyNoise-Enterprise-Threat-Intel-Report-$(date +%Y-%m-%d).json
+      -H "key: $GREYNOISE_TOKEN" \
+      -H "accept: application/json" | jq '.' | cat >> Canarytoken-GreyNoise-Enterprise-Threat-Intel-Report-$(date +%Y-%m-%d).json
 done
+} > /dev/null 2>&1
